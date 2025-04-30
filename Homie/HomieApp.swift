80fe687d1960f7cf46cc8d5956c8babc6b0f7ac4
Sriram P H <dukeonline98@outlook.com>
2025-04-30 05:29:00 -0400

    Initial Commit

---
+++ Homie/HomieApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomieApp.swift
+//  Homie
+//
+//  Created by Sriram P H on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct HomieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HomieTests/HomieTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomieTests.swift
+//  HomieTests
+//
+//  Created by Sriram P H on 4/30/25.
+//
+
+import Testing
+@testable import Homie
+
+struct HomieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
